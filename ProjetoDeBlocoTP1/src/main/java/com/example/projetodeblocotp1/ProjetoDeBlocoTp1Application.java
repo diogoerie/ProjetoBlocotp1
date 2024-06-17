cc02d5f14faf953afd1a527ee7b56c31c7c50d5f
diogoerie <119828335+diogoerie@users.noreply.github.com>
2024-06-17 13:39:08 -0300

    Add files via upload

---
+++ ProjetoDeBlocoTP1/src/main/java/com/example/projetodeblocotp1/ProjetoDeBlocoTp1Application.java
@@ -0,0 +1,13 @@
+package com.example.projetodeblocotp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeBlocoTp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoDeBlocoTp1Application.class, args);
+    }
+
+}
---
+++ ProjetoDeBlocoTP1/src/test/java/com/example/projetodeblocotp1/ProjetoDeBlocoTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetodeblocotp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeBlocoTp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
